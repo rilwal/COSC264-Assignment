471a858d038d42559dbdfbb9307c0c85e4618519
Ryan Terrence McLaughlin <rtm48@cs14044bn.canterbury.ac.nz>
2017-07-26 15:53:45 +1200

    Added some packet code.

---
+++ common/include/packet.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+
+struct PacketHeader {
+    int magic_number;
+    int type;
+    int seq_number;
+    int data_length;
+};
+
---
+++ common/src/packet.cpp
@@ -0,0 +1,10 @@
+#include <packet.hpp>
+
+
+int send(uint8_t* data, size_t length) {
+    PacketHeader header = {};
+    
+
+}
+
+
